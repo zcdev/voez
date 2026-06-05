d0a315867b2ba44927e36d83c8168fe1e907d45a
zcdev <zcdev@hotmail.com>
2026-06-05 09:23:56 -0700

    chore(assets): add playlist data, images, and songs

---
+++ src/app/lib/data/playlist.ts
@@ -0,0 +1,37 @@
+export const playlist = [
+    {
+        "number": 1, // For the order on the playlist
+        "name": "Light Year", // For soundtrack & image
+        "moods": ["epic", "empowering", "marvelous"], // Red
+    },
+    {
+        "number": 2,
+        "name": "Raindrops",
+        "moods": ["joyful", "happy", "playful"], // Orange
+    },
+    {
+        "number": 3,
+        "name": "Kaleidoscope",
+        "moods": ["hopeful", "optimistic", "uplifting"], // Yellow
+    },
+    {
+        "number": 4,
+        "name": "Over the River",
+        "moods": ["content", "serene", "peaceful"], // Green
+    },
+    {
+        "number": 5,
+        "name": "Farewell Symphonys",
+        "moods": ["melancholy", "longing", "sorrowful"], // Blue
+    },
+    {
+        "number": 6,
+        "name": "Passage of Time",
+        "moods": ["reflective, nostalgic, resilient"], // Navy
+    },
+    {
+        "number": 7,
+        "name": "Labyrinth",
+        "moods": ["romantic", "dreamy", "enchanting"], // Purple
+    },
+];
